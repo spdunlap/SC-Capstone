93b1e0ea2c40ed8854420cf40d57531d59a8c061
Spencer Dunlap <spencerdunlap@me.com>
2021-08-26 20:40:49 -0500

    edits

--- store/Links.js
+++ store/Links.js
@@ -2,5 +2,6 @@ export default [
   { title: "Home", text: "Home" },
   { title: "Pay", text: "Pay" },
   { title: "Properties", text: "Properties" },
-  { title: "Maintenance", text: "Maintenance" }
+  { title: "Maintenance", text: "Maintenance" },
+  { title: "Prospect", text: "Prospect" }
 ];
