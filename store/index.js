c4e4c7d2526b57d3a2d1b56a6e206f9475038441
Spencer Dunlap <spencerdunlap@me.com>
2021-08-26 20:37:43 -0500

    edits

--- store/index.js
+++ store/index.js
@@ -3,4 +3,4 @@ export { default as Links } from "./Links";
 export { default as Maintenance } from "./Maintenance";
 export { default as Pay } from "./Pay";
 export { default as Properties } from "./Properties";
-export { default as Prospects } from "./Prospects";
+export { default as Prospect } from "./Prospect";
